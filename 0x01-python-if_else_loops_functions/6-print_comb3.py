569fb016767b697f14cab7b487d90d2f8342406e
Rational-Being <overcomer021@gmail.com>
2023-10-03 11:44:57 -0700

    6-print_comb3.py

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 for f_digit in range(0, 10):
     for s_digit in range(f_digit, 10):
-       if f_digit == 8:
+       if f_digit == 8 and s_digit == 9:
           break
        else:
            print("{}{}, ".format(f_digit, s_digit), end="")
