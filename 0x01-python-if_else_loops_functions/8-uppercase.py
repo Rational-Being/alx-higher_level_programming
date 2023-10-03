1a54279692dcc92eab3c70c943cab56676e51e39
Rational-Being <overcomer021@gmail.com>
2023-10-03 14:18:18 -0700

    8-uppercase.py

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -6,6 +6,5 @@ def uppercase(str):
         var = ord(c)
         if var >= 97 and var <= 122:
             var = var - 32
-        print(f"{var}", end="")
-        #print("{:c}".format(var), end="")
+        print("{:c}".format(var), end="")
     print()
