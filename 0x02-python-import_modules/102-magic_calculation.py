687ae815a73dc7aa4f4822e0ff22c5e24ebf5c3e
Rational-Being <overcomer021@gmail.com>
2023-10-07 23:45:50 -0700

    102-magic_calculation.py

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
 
-def magic_calculator(a, b):
-    from magic_calculator_102 import add, sub
+def magic_calculation(a, b):
+    from magic_calculation_102 import add, sub
 
     if a < b:
         c = add(a, b)
