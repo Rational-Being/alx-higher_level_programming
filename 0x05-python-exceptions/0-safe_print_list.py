c331e49ee0224bb0b1aa8aa5d8c970bca61f3423
Rational-Being <overcomer021@gmail.com>
2023-10-23 19:35:16 -0700

    whatwrong

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,10 +3,11 @@
 
 def safe_print_list(my_list=[], x=0):
     count = 0
-    for i in range(x):
+    for i in my_list:
         try:
             print("{:d}".format(my_list[i]), end="")
             count += 1
         except (ValueError, TypeError, IndexError):
             continue
+    print()
     return (count)
