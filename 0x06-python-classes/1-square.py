a5120e551d790c7659fa364ea9338f39d0d0fcde
Rational-Being <overcomer021@gmail.com>
2023-10-24 20:07:06 -0700

    1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+@author: knowledge seeker
+"""
+
+
+class Square:
+    """
+    a class that defines a square by size
+    """
+
+    def __init__(self, size):
+        """
+        Initailizes the square class
+        Args: size - a private instance that represents the size
+        """
+        self.__size = size
