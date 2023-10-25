890cea1a7ec6d80259524bef3a832fad29ead1ac
Rational-Being <overcomer021@gmail.com>
2023-10-24 20:00:09 -0700

    0-square.py

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+function of a square
+@author: knowledge seeker
+"""
+
+
+class Square:
+    """
+    class square has no attibute or purpose
+    """
+
+    pass
---
+++ 0x06-python-classes/format.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+@author: knowledge seeker
+"""
+
+
+class :
+ """
+ 
+ """
+ 
+ 
+ 
+ 
+ 
+    def __init__(self, size):
+     """
+     
+     """
+     
+     
+     
+     
+     
+     
+     
+     
+     
