876710c2a9254e74a57ba28602f64d544e3052e5
Rational-Being <overcomer021@gmail.com>
2023-10-24 20:49:29 -0700

    6-square.py

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -10,7 +10,7 @@ class Square:
     a class that defines a sqaure
     """
 
-    def __init__(self, size=0):
+    def __init__(self, size=0, position=(0, 0)):
         """
         Initailizes the square class
         Args:
@@ -23,11 +23,16 @@ class Square:
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
-        self.__size = size
+        self.size = size
+        self.position = position
 
     @property
     def size(self):
         return self.__size
+
+    @property
+    def position(self):
+        return self.__position
 
     @size.setter
     def size(self, value):
@@ -35,16 +40,30 @@ class Square:
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
-        self.__size = size
+        self.__size = value
+
+    @position.setter
+    def position(self, value):
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__position = value
 
     def area(self):
         return self.__size**2
 
     def my_print(self):
-        m_p_s = self.__size  # my_print_size
-        if m_p_s == 0:
+        SS = self.__size  # self.__size
+        SP = self.position
+        if SS == 0:
             print()
-        for i in range(m_p_s):
-            for j in range(m_p_s):
-                print("#", end="")
-            print()
+        else:
+            for i in range(SP[1]):
+                print()
+            for i in range(SS):
+                for j in range(SP[0]):
+                    print(end="")
+                for k in range(SS):
+                    print("#", end="")
+                print()
