5f9de341e8ff419cb6a9bb06d33bec011c4e76c1
Rational-Being <overcomer021@gmail.com>
2023-10-31 22:09:55 -0700

    0-rectangle.py

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+
+This module satisfies the task which name the file is named
+
+@author: knowledge seeker
+
+"""
+
+
+class Rectangle:
+    """
+    empty calss that represent a rectangle
+    """
+
+    pass
---
+++ 0x08-python-more_classes/format.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+
+This module satisfies the task which name the file is named
+
+@author: knowledge seeker
+
+"""
+
+def add_integer(a, b=98):
+    """
+    Return the sum of two integers
+    Args: 
+        a: takes int
+        b: takes int
+    Raises:
+        TypeError: if arg not int
+    """
+    
+    
+    
+    
+    
+    
