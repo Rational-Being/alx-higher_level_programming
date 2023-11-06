6a88d7aea2b8a5c41d7bd3d275156019486851e8
Rational-Being <overcomer021@gmail.com>
2023-11-06 06:07:32 -0800

    3-is_kind_of_class.py

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+""""
+created by:knowlede seeker
+"""
+
+
+def is_same_class(obj, a_class):
+    """
+    returns true object of an obj is an instance
+    """
+    return isinstance(obj, a_class)
