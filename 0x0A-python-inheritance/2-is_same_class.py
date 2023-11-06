9a73172049bdd0cd88ac6f5279572a181a58fad1
Rational-Being <overcomer021@gmail.com>
2023-11-06 06:04:10 -0800

    2-is_same_class.py

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+""""
+created by:knowlede seeker
+"""
+
+
+def is_same_class(obj, a_class):
+    """
+    returns true object of an obj is an instance
+    """
+    return type(obj) == a_class
