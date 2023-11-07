c18cfa6f6cf51bab80f7df28cc5cd58ff0607a06
Rational-Being <overcomer021@gmail.com>
2023-11-06 20:14:18 -0800

    11-square.py

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+""""
+created by:knowlede seeker
+"""
+
+BaseGeometry = __import__("7-base_geometry").BaseGeometry
+
+
+class Square(BaseGeometry):
+    """
+    calculates square
+    """
+
+    def __init__(self, width, height):
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def __str__(self):
+        return "[Square]" + str(self.__size) + "/" + str(self.__size)
+
+    def area(self):
+        return self.__size ** 2
