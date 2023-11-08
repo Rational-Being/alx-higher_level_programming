1fe9d13f86ae4e835a7e62ea1c80051cd0edeb0e
Rational-Being <overcomer021@gmail.com>
2023-11-08 17:56:29 +0000

    0-read_file.py

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+
+created by: knowlegde seeker
+
+"""
+
+def read_file(filename=""):
+    """
+    prints a text file
+    """
+    with open(filename, encoding="utf-8") as f:
+       print(f.read(), end="")
+
+
+
+
+
+
+
+
+
+
+
+
+'
+
+
+
+
+
+
