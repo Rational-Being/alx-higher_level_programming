67dd73ff5920f78a69f073522c25daf458855c4d
Rational-Being <overcomer021@gmail.com>
2023-11-09 01:43:06 +0000

    5-

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -12,5 +12,5 @@ def save_to_json_file(my_obj, filename):
     saves to a jason file
     
     """
-    with open(filename, mode='w', encoding='utf-8' as file:
+    with open(filename, mode='w', encoding='utf-8') as file:
         return file.write(json.dumps(my_obj))
