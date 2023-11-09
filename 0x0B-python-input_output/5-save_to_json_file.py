d42eaecf3aa5465ef57470ac8bdbe248f500f381
Rational-Being <overcomer021@gmail.com>
2023-11-09 01:34:30 +0000

    6-load_from_json_file.py

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+
+created by: knowlegde seeker
+
+"""
+
+import json
+
+def save_to_json_file(my_obj, filename):
+    """
+    saves to a jason file
+    
+    """
+    with open(filename, mode='w', encoding='utf-8' as file:
+        return file.write(json.dumps(my_obj))
+    return json.loads(my_str)
