3b874cebaa5aef574a2967eff590b8691389e63f
Rational-Being <overcomer021@gmail.com>
2023-11-09 01:22:58 +0000

    4-from_json_string.py

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+
+created by: knowlegde seeker
+
+"""
+
+import json
+
+def from_json_string(my_str):
+    """
+    prints a text file
+    """
+    return json.loads(my_str)
