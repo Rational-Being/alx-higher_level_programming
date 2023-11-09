e99c5e7e8bc65df076afe47d11050479a427740f
Rational-Being <overcomer021@gmail.com>
2023-11-09 02:22:16 +0000

    100-append_after.py

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+
+created by: knowlegde seeker
+
+"""
+
+def append_after(filename="", search_string="", new_string="")
+    """
+    prints a text file
+    """
+    with open(filename) as f:
+        for line in f:
+            text += line
+            if search_String in line:
+                text += new_string
+    with open(filename, "w") as f:
+        w.write(text)
