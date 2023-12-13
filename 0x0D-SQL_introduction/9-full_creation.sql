8830ca08cc33236c28890bc8b565ca913904ce3a
Rational-Being <overcomer021@gmail.com>
2023-12-12 20:52:30 -0800

    9-full_creation.sql

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,4 +1,5 @@
- -- creates a second table
+-- creates a second table
+-- and add values
 
 CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
 INSERT INTO second_table (id, name, score) VALUES (1, 'John', 10);
