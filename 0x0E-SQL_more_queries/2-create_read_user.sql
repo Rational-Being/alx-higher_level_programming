8e0d81e2a2cb63487b5fb9e719f8e3396d25cf7f
Rational-Being <overcomer021@gmail.com>
2023-12-13 18:56:14 -0800

    4-never_empty.sql

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -5,5 +5,6 @@ CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 -- create user
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 -- gratnt priveleges
-GRANT SELECT ON *.* TO 'user_0d_2'@'localhost';
+GRANT USAGE ON *.* TO 'user_0d_2'@'localhost';
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
 FLUSH PRIVILEGES;
