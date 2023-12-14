0dfd0f9279cee5e84660c04dfe72916f3841c8cd
Rational-Being <overcomer021@gmail.com>
2023-12-13 18:42:41 -0800

    2-create_read_user.sql

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -2,7 +2,5 @@
 
 -- create user
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-
 -- gratnt priveleges
-
-GRANT PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,8 @@
+-- script creates databse user and grant all privelegs
+
+-- create databse
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+-- create user
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+-- gratnt priveleges
+GRANT SELECT ON *.* TO 'user_0d_2'@'localhost';
