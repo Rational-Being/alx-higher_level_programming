32666298e677420d49ac314769f58d89220f7172
Rational-Being <overcomer021@gmail.com>
2024-01-08 21:54:04 +0100

    6-multi_languages_loop.js

---
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -0,0 +1,5 @@
+#!/usr/bin/node
+
+const arg = 'No argument';
+
+console.log(process.argv[2] || arg);
---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+
+const myVar = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+for (const mVar of myVar) {
+  console.log(mVar);
+}
