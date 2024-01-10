a9f2181a009b2eabd22126f65f2271ead7ef7235
Rational-Being <overcomer021@gmail.com>
2024-01-10 20:53:14 +0100

    6-square.js

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -3,15 +3,17 @@
 const square = require('./5-square');
 
 class Square extends square {
-  constructor (size) {
+  constructor(size) {
     super(size, size);
   }
 
-  charPrint (c) {
-    if (!c) {
+  charPrint(c) {
+    if (c === undefined) {
       c = 'X';
     }
-    console.log(c.repeat(this.width));
+    for (let i = 0; i <= this.width; i++) {
+      console.log(c.repeat(this.width));
+    }
   }
 }
 
