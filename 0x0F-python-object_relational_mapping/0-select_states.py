5ff202b188ca642434fae9c796892ce8d1f87028
Rational-Being <overcomer021@gmail.com>
2024-01-12 15:40:40 +0100

    0-select_states.py

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,12 +1,17 @@
 #!usr/bin/python3
 
+    """ 
+    This script conects to a database, create a cursor object
+    selct rows, print them and 
+    """
+
 import MySQLdb
 from sys import argv
 
 if __name__ == '__main__':
     args = argv
     
-    connect_db = MySQLdb.connect(host='localhost', user=args[1], passwd=args[2], db=args[3, port=3306])
+    connect_db = MySQLdb.connect(host='localhost', user=args[1], passwd=args[2], db=args[3], port=3306)
     
     cur = connect_db.cursor()
     cur.execute('SELECT * FROM states ORDER BY states.id;')
@@ -18,4 +23,5 @@ if __name__ == '__main__':
     
     cur.close()
     connect_db.close()
+
     
