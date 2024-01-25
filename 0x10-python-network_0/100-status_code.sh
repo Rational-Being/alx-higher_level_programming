a7a454f47fe7a8c3d0a5b45658bc237af1356c9c
Rational-Being <overcomer021@gmail.com>
2024-01-25 20:04:24 +0100

    100-status_code.sh

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #a bash script that send a request to a URL passed as anargument and siplays only the status code of the response
-curl -s -o /dev/null -w "%{http_code}" "$1"
+curl -s -o /dev/null -w "%{http_code}" "$1" 
