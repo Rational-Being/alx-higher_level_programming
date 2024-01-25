60e130c72ad22d452f991e97c957f8907dac4b22
Rational-Being <overcomer021@gmail.com>
2024-01-25 19:14:51 +0100

    added new line

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-#a bash script that takes in a URL as an argument, sends a POST requaest to the URL
-#and displays the body of the response
+#a bash script that takes in a URL as an argument, sends a POST requaest to the URL and displays the body of the response
 curl -sd "email=test@gmail.com&subject=I will always be here for PLD" "$1"
