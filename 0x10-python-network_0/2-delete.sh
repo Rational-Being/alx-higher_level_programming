2899586ed3ebdce53a94b22885d9205e6138fa8e
Rational-Being <overcomer021@gmail.com>
2024-01-25 19:07:33 +0100

    mission accomplished

--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-#a bash scripts that send a DELETE request to the URL passed as the first argument
-#and dispalys the body of the response
+#a bash scripts that send a DELETE request to the URL passed as the first argument and dispalys the body of the response
 curl -sX DELETE "$@"
