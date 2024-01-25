81be442350e4347f67a53e878e973ebd4f2f923e
Rational-Being <overcomer021@gmail.com>
2024-01-25 20:10:01 +0100

    101-post_json.sh

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #a bash script that sends a JSON POST request ot a URL passed as the forst argument, and displays the body of the response
-curl -sH 'Content-Type: application/json' -d "@${2}" $@
+curl -sH 'Content-Type: application/json' -d @"$2" $@
