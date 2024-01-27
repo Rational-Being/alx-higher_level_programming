91e89b19345b43529222c8c9775c8e8082f03253
Rational-Being <overcomer021@gmail.com>
2024-01-26 20:29:07 -0800

    4-hbtn_status.py

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
+
 """
 this script fetches the url provided in the code
 using the request package
 """
+
 from requests import get
+
 if __name__ == "__main__":
     response = get("https://intranet.hbtn.io/status")
     print("Body response:")
