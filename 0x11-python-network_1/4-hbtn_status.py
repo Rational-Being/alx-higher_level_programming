a3e602cd40c33b8a588831b1667c112a552bab58
Rational-Being <overcomer021@gmail.com>
2024-01-26 20:33:52 -0800

    4-hbtn_status.py_tab

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 
 """
-this script fetches the url provided in the code
-using the request package
+this script fetches the url provided in the code using the request package
+the package requests must be use
+the body of the request should display in tabular form
 """
 
 from requests import get
