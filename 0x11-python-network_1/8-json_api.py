ebf35fa0aef9d55aa71b22652c5c24b352f24d2b
Rational-Being <overcomer021@gmail.com>
2024-01-27 20:53:58 -0800

    8-json_api.py

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -3,7 +3,27 @@
 a python script that takes in a letter and send a POST request
 to a URL sith the letter as a parameter
 """
-form requests import post
+from requests import post
+from requests import codes
+from sys import argv
 
 if __name__ == "__main__":
+    url = 'http://0.0.0.0:5000/search_user'
+    if len(argv) < 2:
+        arg = {"q": ""}
+    else:
+        arg = {"q": argv[1]}
+
+    response = post(url, data=arg)
     
+    try:
+        json = response.json()
+        if len(obj) == 0:
+            print('No result')
+        else:
+            print('[{}] {}'.format(json['id'], json['name']))
+    except:
+            print('Not a valid JSON')
+
+
+
